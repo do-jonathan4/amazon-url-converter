0a4313633e380142676fa04c6beee01af976e885
do-jonathan4 <d.johnt45@gmail.com>
2023-07-05 18:27:26 -0700

    conditional url routes, disable empty link

--- main.js
+++ main.js
@@ -3,10 +3,22 @@ const [originalURL, newURL] = document.querySelectorAll('input')
 
 convert.addEventListener('click', e => {
     const str = originalURL.value
-    const vjk = str.lastIndexOf('jk=')
-    const advn = str.lastIndexOf('&advn=')
+    if (str === '') return
 
-    newURL.value = "https://www.indeed.com/viewjob?" + str.slice(vjk, advn)
+    const vjk = str.lastIndexOf('jk=')
+    const advn = () => {
+        if (str.lastIndexOf('&advn=') !== -1) {
+            return str.lastIndexOf('&advn=')
+        } else {
+            return str.lastIndexOf('&q=')
+        }
+    }
+    newURL.value = "https://www.indeed.com/viewjob?" + str.slice(vjk, advn())
     go.parentElement.href = newURL.value
+    go.parentElement.target = "_blank"
 })
 
+paste.addEventListener('click', e => {
+    navigator.clipboard.readText()
+        .then(x => originalURL.value = x)
+})
