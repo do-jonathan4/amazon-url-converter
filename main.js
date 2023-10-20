57c5bc1d9f44c98f660bf64f8f51d5994173a205
do-jonathan4 <d.johnt45@gmail.com>
2023-10-20 14:15:51 -0700

    removes productID

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
-const [paste, convert, go] = document.querySelectorAll('button')
-const [originalURL, newURL] = document.querySelectorAll('input')
+const [paste, convert, go] = document.querySelectorAll("button");
+const [originalURL, newURL] = document.querySelectorAll("input");
 
-window.onload = () => originalURL.focus()
+window.onload = () => originalURL.focus();
 
-convert.addEventListener('click', e => {
-    const str = originalURL.value
-    if (str === '' || !str.includes('amazon')) return
+convert.addEventListener("click", (e) => {
+  const str = originalURL.value;
+  if (str === "" || !str.includes("amazon")) return;
 
-    const dp = str.lastIndexOf('dp/')
-    newURL.value = "https://www.amazon.com/" + str.slice(dp, dp+13)
+  const dp =
+    str.lastIndexOf("dp/") === -1
+      ? str.lastIndexOf("product/") + 8
+      : str.lastIndexOf("dp/") + 3;
+  const dpVal = str.slice(dp, dp + 10);
+  newURL.value = "https://www.amazon.com/dp/" + dpVal;
 
-    go.parentElement.href = newURL.value
-    go.parentElement.target = "_blank"
-})
+  go.parentElement.href = newURL.value;
+});
 
-paste.addEventListener('click', e => {
-    navigator.clipboard.readText()
-        .then(x => originalURL.value = x)
-})
+paste.addEventListener("click", (e) => {
+  navigator.clipboard.readText().then((x) => (originalURL.value = x));
+});
